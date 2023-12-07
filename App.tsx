8aaa706913559707e5187fd3ccb6ca005fe0a76a
JoaoVDS21 <joao.programmerS2@gmail.com>
2023-12-06 23:18:03 -0300

    importing fonts to app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    bold: require("./assets/fonts/Poppins-Bold.ttf"),
+    extrabold: require("./assets/fonts/Poppins-ExtraBold.ttf"),
+    light: require("./assets/fonts/Poppins-Light.ttf"),
+    medium: require("./assets/fonts/Poppins-Medium.ttf"),
+    regular: require("./assets/fonts/Poppins-Regular.ttf"),
+    semibold: require("./assets/fonts/Poppins-SemiBold.ttf")
+  })
+
+  const onLayoutRootView = useCallback(async() => {
+    if(fontsLoaded){
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if(!fontsLoaded) {
+    return null;
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textStyle}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +40,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle: {
+    fontFamily: "regular",
+    fontSize: 20
+  }
 });
